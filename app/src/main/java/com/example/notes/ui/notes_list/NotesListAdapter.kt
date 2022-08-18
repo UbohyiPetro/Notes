30bd185657317c6c98e9e901e8b5d18764a0e0f7
petro.ubohyi <petro.ubohyi@globallogic.com>
2022-08-18 20:20:18 +0300

    implement database, create note repository and model for note entity and note change recycler adapter to list adapter

--- app/src/main/java/com/example/notes/ui/notes_list/NotesListAdapter.kt
+++ app/src/main/java/com/example/notes/ui/notes_list/NotesListAdapter.kt
@@ -1,28 +1,39 @@
 package com.example.notes.ui.notes_list
 
-import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.findNavController
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.notes.R
+import com.example.notes.ui.notes_list.model.Note
 import kotlinx.android.synthetic.main.note_item.view.*
 
-class NotesListAdapter() : RecyclerView.Adapter<NotesListAdapter.NotesListViewHolder>() {
+class NotesListAdapter : ListAdapter<Note, NotesListAdapter.NotesViewHolder>(
+    object : DiffUtil.ItemCallback<Note>() {
+        override fun areItemsTheSame(oldItem: Note, newItem: Note): Boolean {
+            return oldItem.id == newItem.id
+        }
 
-    private var notesList: MutableList<Note> = mutableListOf()
+        override fun areContentsTheSame(oldItem: Note, newItem: Note): Boolean {
+            return oldItem.id == newItem.id
+        }
 
-    class NotesListViewHolder(view: View) : RecyclerView.ViewHolder(view)
+    }
+) {
+    inner class NotesViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NotesListViewHolder {
-        val itemView = LayoutInflater.from(parent.context)
-            .inflate(R.layout.note_item, parent, false)
-        return NotesListViewHolder(itemView)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NotesViewHolder {
+        return NotesViewHolder(
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.note_item, parent, false)
+        )
     }
 
-    override fun onBindViewHolder(holder: NotesListViewHolder, position: Int) {
-        val note = notesList[position]
+    override fun onBindViewHolder(holder: NotesViewHolder, position: Int) {
+        val note = currentList[position]
         holder.itemView.apply {
             setOnClickListener {
                 findNavController().navigate(
@@ -34,16 +45,4 @@ class NotesListAdapter() : RecyclerView.Adapter<NotesListAdapter.NotesListViewHo
             tvDescription.text = note.description
         }
     }
-
-    override fun getItemCount(): Int {
-        return notesList.size
-    }
-
-    @SuppressLint("NotifyDataSetChanged")
-    fun setList(notes: MutableList<Note>) {
-        notesList = notes
-        notifyDataSetChanged()
-    }
-
-
 }
